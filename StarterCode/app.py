c0d0c4594a384975761974d6a9d32fe9ad5c4a08
tran247 <75042393+tran247@users.noreply.github.com>
2021-04-23 11:19:49 -0400

    added basic flask

---
+++ StarterCode/app.py
@@ -0,0 +1,16 @@
+#flask 
+
+from flask import Flask, jsonify, render_template
+#import pyscopg2
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify(jsondata)
+
+if __name__ =="__main__":
+    app.run(debug=True)
+    
+
+

4d6ba9a3a7a89214e9ff401034987d28355e0726
catherinedhwang <catherine.d.hwang@gmail.com>
2021-04-29 20:23:18 -0400

    final

--- StarterCode/app.py
+++ StarterCode/app.py
@@ -1,28 +1,39 @@
 #flask 
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+import sys
+import json
+from decimal import Decimal
+import simplejson
+
+
 
 app = Flask(__name__)
 
-# #Create engine and connection
-# engine = create_engine('postgresql://postgres:admin@localhost:5432/DBZ')
-# conn = engine.connect()
-# table = pd.read_sql("SELECT * FROM database", conn)
-# data_df = pd.DataFrame(table)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/project_2'
+
+db = SQLAlchemy(app)
+
+ 
+@app.route('/data')
+
+def data():
+    # USE YOUR OWN PASSWORD, DBNAME HERE! 
+    con = psycopg2.connect("host='localhost' dbname='project2' user='postgres' password='asdf'")  
+    cur = con.cursor()
+    cur.execute("""select * from database""")
+    data = [col for col in cur]
+    cur.close()
+    return jsonify(data)
+    
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# @app.route('/data')
-# def data():
-
-# def home():
-#     return jsonify(jsondata)
-
 if __name__ =="__main__":
     app.run(debug=True)
-
-
-
--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,97 +1,125 @@
-d3.json("static/js/samples.json").then(function createPlotly(data) {
-    // console.log(data);
-    var testid = data.names;
-    console.log(testid);
-  
-    // create dynamic dropdown menu
-    d3.select("#selDataset")
-      .selectAll("option")
-      .data(testid)
-      .enter()
-      .append("option")
-      .html(function(d) {
-        return `<option>${d}</option`;
-      });
-  
-    // Retrive the selected option and use it to get index
-    var dropdownMenu = d3.select("#selDataset");
-    var dropdownValue = dropdownMenu.property("value");
-    var index = testid.indexOf(dropdownValue);
-  
-    // Create a dynamic demographic info
-    d3.select("#sample-metadata").html("");
-    d3.select("#sample-metadata")
-      .selectAll("p")
-      .data(Object.entries(data.metadata[index]))
-      .enter()
-      .append("p")
-      .html(function(d) {
-        return `${d[0]} : ${d[1]}`;
-      });
-  
-    console.log(Object.entries(data.metadata[index]));
-  
-  
-    // Create a bar graph using index
-    var defaultsampleData = data.samples[index].sample_values
-    .slice(0, 10)
-    .reverse();
-  var defaultotuData = data.samples[index].otu_ids.slice(0, 10).reverse();
-  var defaultotuLabels = data.samples[index].otu_labels.slice(0, 10).reverse();
-  var defaultyxis = defaultotuData.map(a => "OTU" + a);
-  
-  var bardata = [
-    {
-      x: defaultsampleData,
-      y: defaultyxis,
-      type: "bar",
-      orientation: "h",
-      text: defaultotuLabels
-    }
-  ];
-  
-  var barLayout = {
-    title: "TOP10 Sample Values",
-    xaxis: { title: "Sample Values" },
-    yaxis: { title: "OTU ID" }
-  };
-  
-  Plotly.newPlot("bar", bardata, barLayout);
-  
-   // Create a bubble chart
-   var bubbleData = [
-    {
-      x: data.samples[index].otu_ids,
-      y: data.samples[index].sample_values,
-      mode: "markers",
-      text: data.samples[index].otu_labels,
-      marker: {
-        size: data.samples[index].sample_values,
-        color: data.samples[index].otu_ids,
-        colorscale: "Viridis"
+
+d3.json("http://127.0.0.1:5000/data").then((data) => {
+    console.log(data);
+// FERTILITY
+    var fertility= data.filter(sampleObj => sampleObj[2] === "FERTILITY"); 
+    var time = [];
+    var fertilityValue = [];
+      for (var i = 0; i < fertility.length; i++) {
+            time.push(fertility[i][5]);
+            fertilityValue.push(fertility[i][6]);
       }
-    }
-  ];
-  
-  var bubbleLabels = {
-    title: "Bacteria Cultures Per Sample",
-    xaxis: { title: "OTU ID" },
-    yaxis: { title: "Sample Values" }
-  };
-  
-  Plotly.newPlot("bubble", bubbleData, bubbleLabels);
-  // When different test ID is selected, call an function optionChanged
-  d3.select("#selDataset").on("change", optionChanged1);
-  
-  function optionChanged1() {
-  console.log("Different item was selected.");
-  var dropdownMenu = d3.select("#selDataset");
-  var dropdownValue = dropdownMenu.property("value");
-  console.log(`Currently test id ${dropdownValue} is shown on the page`);
-  
-  // Update graph
-  createPlotly(data);
-  
-  }
-  
-  });
+      // console.log("time=", time);
+      // console.log("fertilityValue=", fertilityValue);
+// HHDI GROSS 
+    var hhdiGross= data.filter(sampleObj => sampleObj[3] === "GROSSADJ"); 
+    var hhdiGrossValue=[];
+    for (var i = 0; i < hhdiGross.length; i++) {
+      hhdiGrossValue.push(hhdiGross[i][6]);
+      }
+      // console.log("hhdiGrossValue=", hhdiGrossValue);
+// HHDI NET
+    var hhdiNet= data.filter(sampleObj => sampleObj[3] === "NET"); 
+    var hhdiNetValue=[];
+    for (var i = 0; i < hhdiNet.length; i++) {
+      hhdiNetValue.push(hhdiNet[i][6]);
+      }
+      // console.log("hhdiNetValue=", hhdiNetValue);
+// HOUSING COST
+    var housingCost = data.filter(sampleObj => sampleObj[2] === "HOUSECOST"); 
+    var housingCostValue =[];
+    for (var i = 0; i < housingCost.length; i++) {
+      housingCostValue.push(housingCost[i][6]);
+      }
+      // console.log("housingCostValue=", housingCostValue);
+// EDUCATION
+    var education = data.filter(sampleObj => sampleObj[2] === "EDUTRY"); 
+    var educationValue=[];
+    for (var i = 0; i < education.length; i++) {
+      educationValue.push(education[i][6]);
+      }
+      // console.log("educationValue=", educationValue);
+// NATIONAL DEBT
+    var natlDebt = data.filter(sampleObj => sampleObj[2] === "HHDEBT"); 
+    var debtTime = [];
+    var natlDebtValue= [];
+    for (var i = 0; i < natlDebt.length; i++) {
+      debtTime.push(natlDebt[i][5]);
+      natlDebtValue.push(natlDebt[i][6]);
+      }
+      // console.log("natlDebtValue=", natlDebtValue);
+
+      // LINE GRAPH FOR FERTILITY     
+      var trace1 = {
+      x: time,
+      y: fertilityValue,
+      type: "line"
+      };
+      var data = [trace1];
+      var layout = {
+      title: "Fertility Rate over Time"
+      }
+      Plotly.newPlot("bar", data, layout);
+
+      // LINE GRAPH FOR HHDI (GROSS)     
+      var trace1 = {
+      x: time,
+      y: hhdiGrossValue,
+      type: "line"
+      };
+      var data = [trace1];
+      var layout = {
+      title: "Disposable Income over Time (GROSS)"
+      }
+      Plotly.newPlot("bar1", data, layout);
+
+       // LINE GRAPH FOR NATL DEBT 
+       var trace1 = {
+      x: debtTime,
+      y: natlDebtValue,
+      type: "line"
+      };
+      var data = [trace1];
+      var layout = {
+      title: "National Debt over Time"
+      }
+      Plotly.newPlot("bar2", data, layout);
+
+      // LINE GRAPH FOR HOUSING COST   
+      var trace1 = {
+      x: time,
+      y: housingCostValue,
+      type: "line"
+      };
+      var data = [trace1];
+      var layout = {
+      title: "Housing Cost over Time"
+      }
+      Plotly.newPlot("bar3", data, layout);
+
+      // LINE GRAPH FOR EDUCATION LEVEL  
+      var trace1 = {
+      x: time,
+      y: educationValue,
+      type: "line"
+      };
+      var data = [trace1];
+      var layout = {
+      title: "Education Level over Time"
+      }
+      Plotly.newPlot("bar4", data, layout);
+
+       // LINE GRAPH FOR HHDI (NET)     
+      // var trace1 = {
+      // x: time,
+      // y: hhdiNetValue,
+      // type: "line"
+      // };
+      // var data = [trace1];
+      // var layout = {
+      // title: "Disposable Income over Time (NET)"
+      // }
+      // Plotly.newPlot("bar2", data, layout);
+
+     
+});
